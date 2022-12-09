f5f54d9211798bc343be9653059a72de3c1666ef
Larissa Hoffmann Sebold <larissa.sebold@proway.treina>
2022-12-09 19:55:50 -0300

    atualizaçao larissa

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+from pessoa import Pessoa
+
+pessoas = [
+    Pessoa('Jean', 42, 1.80),
+    Pessoa('Larissa', 28, 1.58),
+    Pessoa('Haiko', 82, 1.75)
+]
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def inicio():
-    return render_template('index.html')
+    return render_template('index.html', pessoas = pessoas, title = 'Home')
 
 @app.route('/novo')
 def novo():
